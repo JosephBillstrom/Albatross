a0b248e1faae2144b3784b08a28e2b3510e4da1e
JosephBillstrom <joseph.billstrom@ndsu.edu>
2016-04-28 16:42:15 -0500

    Level 1 Update 3
    Popup Text script written and added, some cosmetics and bugfixes

---
+++ Albatross/Assets/Scripts/PopupText.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PopupText : MonoBehaviour {
+
+    GameObject[] mayorObjects;
+
+	// Use this for initialization
+	void Start ()
+    {
+        mayorObjects = GameObject.FindGameObjectsWithTag("PopupText");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+	}
+
+    //Assigned to button; clicking will dismiss Mayor Text
+    public void HideText()
+    {
+        foreach (GameObject go in mayorObjects)
+        {
+            go.SetActive(false);
+        }
+    }
+}
